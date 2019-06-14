442f56d297271a26d14c6e2b4a948bb4339269a8
Daniel2242014 <40508304+Daniel2242014@users.noreply.github.com>
2019-06-13 22:45:14 -0300

    Update Titular.sh

--- Actividades/So01001ySo01002/ABM/Titular.sh
+++ Actividades/So01001ySo01002/ABM/Titular.sh
@@ -1,11 +1,12 @@
+
 #!/bin/bash
-		#Imprencion de texto
-		echo "    _____________________________________________"
-    echo " 	  |                                           |"
-		echo "	  |                                           |"
-		echo "	  |          Bienvenidos Al servidor          |"
-		echo "	  |                  por Bit                  |"
-		echo "    |                                           |"
-		echo "	  |___________________________________________|"
-		echo "	  Usuario: $USER" #Muestra el nombre del usuario
-		echo "    Ultimo Ingreso: lastlog  -u root |grep "$USER"| cut -c45-80" #muestra el ultimo ingreso al sistema
+#Imprencion de texto
+echo "_____________________________________________"
+echo "|                                           |"
+echo "|                                           |"
+echo "|          Bienvenidos Al servidor          |"
+echo "|                  por Bit                  |"
+echo "|                                           |"
+echo "|___________________________________________|"
+echo "  Usuario: $USER" #Muestra el nombre del usuario
+echo "  Ultimo Ingreso: $(lastlog  -u root |grep "$USER"| cut -c45-80") #muestra el ultimo ingreso al sistema
