68264b0be6e6dcbaa5f0fe7d4c23ca8084c06369
Daniel2242014 <40508304+Daniel2242014@users.noreply.github.com>
2019-06-13 22:02:52 -0300

    Create Titular.sh

---
+++ Actividades/So01001ySo01002/ABM/Titular.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+		#Imprencion de texto
+		echo "    _____________________________________________"
+    echo " 	  |                                           |"
+		echo "	  |                                           |"
+		echo "	  |          Bienvenidos Al servidor          |"
+		echo "	  |                  por Bit                  |"
+		echo "    |                                           |"
+		echo "	  |___________________________________________|"
+		echo "	  Usuario: $USER" #Muestra el nombre del usuario
+		echo "    Ultimo Ingreso: lastlog  -u root |grep "$USER"| cut -c45-80" #muestra el ultimo ingreso al sistema
