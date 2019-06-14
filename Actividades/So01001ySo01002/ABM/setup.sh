8f375e1c18b6a6e27d4f351dc9cd5cc8496b4387
Daniel2242014 <40508304+Daniel2242014@users.noreply.github.com>
2019-06-13 22:46:29 -0300

    Update setup.sh

--- Actividades/So01001ySo01002/ABM/setup.sh
+++ Actividades/So01001ySo01002/ABM/setup.sh
@@ -26,7 +26,7 @@ ConfiguracionDelAmbienteDeTrabajo() #Funcion encarga de la instalacion
 		cd $carpeta #Nos movemos a /var
 		git clone https://github.com/Daniel2242014/DataConfiguracionABMusuariosSO
 		#Subido en la direcion url que se puede ver en la linea anterior se tiene subido todos los shell script y funciones nesesarias para el correcto funcionamiento de la ABM. De esta forma el usuario no debera tener todos los archivos, solamente el shell setup para la instalacion
-
+		mv /var/DataConfiguracionABMusuariosSO/Titular.sh /etc/profile.d/Titular.sh 
 		touch /etc/profile.d/z_ABMConfiguration.sh #Creamos un archivo de configuracion del PATH en /etc/profile.d
 		echo "export PATH=$PATH:/var/DataConfiguracionABMusuariosSO/" > /etc/profile.d/z_ABMConfiguration.sh
 		PATH="$PATH:/var/DataConfiguracionABMusuariosSO/" #cambia PATH, con SH no te permite hacerlo, por eso debe usar source el usuario 
@@ -50,12 +50,16 @@ ConfiguracionDelAmbienteDeTrabajo() #Funcion encarga de la instalacion
 			useradd Administrador 2> /dev/null #Este usuario se crea para el acceso de los administradores al sistema 
 			echo "admin_bit2019" | passwd --stdin Administrador > /dev/null #Se le asigna la contraseña al administrador
 		fi
+		
 		chmod 700 /var/DataConfiguracionABMusuariosSO 	
+		echo "Bienvenido al servidor del sistema SLTA" > /etc/issue
+		echo "Ingrese su usuario y contraseña" >> /etc/issue
 		echo "Proseso terminado con exito, ejecute setup.sh desde la consola"
 		rm -f ./setup.sh
 	fi
 
 }
+
 
 desinstalar()
 {
@@ -69,6 +73,11 @@ desinstalar()
 			rm -f /etc/profile.d/z_ABMConfiguration.sh #eliminamo el archivo de configuracion del PATH
 		fi
 	
+		if test -f /etc/profile.d/Titular.sh
+		then
+			rm -f /etc/profile.d/Titular.sh #se elimina el titular 
+		fi
+
 		PATH=$(echo $PATH | sed -e 's/:\/var\/DataConfiguracionABMusuariosSO\///g') #Elimina la ublicacion de la inlstalacion del path 
 		export PATH
 		if test $(grep -e "^Operario:" /etc/passwd| wc -l) -eq 1 # si el usuario operario existe 
@@ -85,7 +94,7 @@ desinstalar()
 		then
 			userdel Administrador #Elimina al administrador si exsiste 
 		fi
-			
+		echo "" > /etc/issue	
 		if test -z $1
 		then 
 			echo "Proseso terminado con exito"	
@@ -97,74 +106,80 @@ desinstalar()
 
 if test $(w |tail -$[$(w | wc -l)-2] | grep "sh setup" | wc -l) -eq 0 #comprueba que se ejecute con source 
 then
-	
-	if test $USER = "root" # Debe ser el usuario root quien utilice el shell 
+
+	if test $(git --help 2>/dev/null | wc -l ) -ne 0
 	then
-		if test $(echo $PATH | grep "/var/DataConfiguracionABMusuariosSO/"|wc -l) -eq 1  #revisa que este la ubicacion de la instalacion 
+	
+		if test $USER = "root" # Debe ser el usuario root quien utilice el shell 
 		then
-			clear
-			#Se importan un conjunto de archivos llenos de metodos a utilizar en la ABM
-			source /var/DataConfiguracionABMusuariosSO/lib/lib_menu.sh
-			source /var/DataConfiguracionABMusuariosSO/lib/lib_error.sh
-			source /var/DataConfiguracionABMusuariosSO/lib/DT.sh
-			source /var/DataConfiguracionABMusuariosSO/lib/expiracionUsuario.sh
-			source /var/DataConfiguracionABMusuariosSO/lib/GP.sh
-			source /var/DataConfiguracionABMusuariosSO/lib/GS.sh
-			source /var/DataConfiguracionABMusuariosSO/lib/NDiasHasta.sh
-			source /var/DataConfiguracionABMusuariosSO/lib/pass.sh
-			source /var/DataConfiguracionABMusuariosSO/lib/shell.sh
-			source /var/DataConfiguracionABMusuariosSO/lib/UDI.sh
-			source /var/DataConfiguracionABMusuariosSO/lib/userE.sh
-			source /var/DataConfiguracionABMusuariosSO/lib/fechacal.sh
-			source /var/DataConfiguracionABMusuariosSO/sub_shell/agregarUsuario.sh 
-			source /var/DataConfiguracionABMusuariosSO/sub_shell/ModificarUsuario.sh 
-			source /var/DataConfiguracionABMusuariosSO/sub_shell/eliminarUsuario.sh
-			source /var/DataConfiguracionABMusuariosSO/sub_shell/listarUsuarios.sh 
-			source /var/DataConfiguracionABMusuariosSO/sub_shell/agregarGrupo.sh 
-			source /var/DataConfiguracionABMusuariosSO/sub_shell/ModificarGrupo.sh 
-			source /var/DataConfiguracionABMusuariosSO/sub_shell/EliminarGrupo.sh 
-			source /var/DataConfiguracionABMusuariosSO/sub_shell/listarGrupos.sh 
-			source /var/DataConfiguracionABMusuariosSO/sub_shell/Preferencias.sh 
-			carpetaBase='/var/DataConfiguracionABMusuariosSO'
-
-			respuesta="" #El dato pasado por los return solo puede ser numerico, entonces utilizamos una variable externa donde se cargen las salidas, como si fuera un $? pero con mayor capasidad 
-
-			echo "   _____________________________________________  "
-			echo "   |                                           | "
-			echo "   |                                           | "
-			echo "   |           ABM usuarios y grupos           | "
-			echo "   |                  por Bit                  | "
-			echo "   |                                           | "
-			echo "   |___________________________________________| "
-			echo "" 
-			# se carga un array con los nombre de las opciones del menu 
-			nombres=('Agregar_usuario' 'Modificar_usuarios' 'Eliminar_usuarios' 'Listar_usuarios' 'Agregar_grupo' 'editar_grupo' 'eliminar_grupo' 'Listar_grupo' 'Editar_preferencias' 'Reinstalar' 'Desinstalar')
-			# se carga el nombre de los metodos que llaman dichas opciones 
-			direcionesSetUp=('agregarUsuario' 'ModificarUsuario' 'eliminarUsuarios' 'listarUsuarios' 'agregarGrupo' 'ModificarGrupo' 'eliminarGrupo' 'MenuListarGrupos' 'Preferencias' 'ConfiguracionDelAmbienteDeTrabajo' 'desinstalar')
-
-			menu 'nombres[@]' 'direcionesSetUp[@]' #se llama al metodo menu 
-
-		else
-			echo "   _____________________________________________  "
-			echo "   |                                           | "
-			echo "   |                                           | "
-			echo "   |           ABM usuarios y grupos           | "
-			echo "   |                  por Bit                  | "
-			echo "   |                                           | "
-			echo "   |___________________________________________| "
-			echo "" 
-		
-			echo "debe instalar el softare para utilizarlo" 
-			echo "Desea comenzar el proseso de instalacion? (1= si 0=no)"
-			read de		
-			if test $de -eq 1 2> /dev/null #Comprueba que el dato ingresado sea 1
+			if test $(echo $PATH | grep "/var/DataConfiguracionABMusuariosSO/"|wc -l) -eq 1  #revisa que este la ubicacion de la instalacion 
 			then
-				ConfiguracionDelAmbienteDeTrabajo #se prosesde con la instalacion del sistema 
-			fi	
+				clear
+				#Se importan un conjunto de archivos llenos de metodos a utilizar en la ABM
+				source /var/DataConfiguracionABMusuariosSO/lib/lib_menu.sh
+				source /var/DataConfiguracionABMusuariosSO/lib/lib_error.sh
+				source /var/DataConfiguracionABMusuariosSO/lib/DT.sh
+				source /var/DataConfiguracionABMusuariosSO/lib/expiracionUsuario.sh
+				source /var/DataConfiguracionABMusuariosSO/lib/GP.sh
+				source /var/DataConfiguracionABMusuariosSO/lib/GS.sh
+				source /var/DataConfiguracionABMusuariosSO/lib/NDiasHasta.sh
+				source /var/DataConfiguracionABMusuariosSO/lib/pass.sh
+				source /var/DataConfiguracionABMusuariosSO/lib/shell.sh
+				source /var/DataConfiguracionABMusuariosSO/lib/UDI.sh
+				source /var/DataConfiguracionABMusuariosSO/lib/userE.sh
+				source /var/DataConfiguracionABMusuariosSO/lib/fechacal.sh
+				source /var/DataConfiguracionABMusuariosSO/sub_shell/agregarUsuario.sh 
+				source /var/DataConfiguracionABMusuariosSO/sub_shell/ModificarUsuario.sh 
+				source /var/DataConfiguracionABMusuariosSO/sub_shell/eliminarUsuario.sh
+				source /var/DataConfiguracionABMusuariosSO/sub_shell/listarUsuarios.sh 
+				source /var/DataConfiguracionABMusuariosSO/sub_shell/agregarGrupo.sh 
+				source /var/DataConfiguracionABMusuariosSO/sub_shell/ModificarGrupo.sh 
+				source /var/DataConfiguracionABMusuariosSO/sub_shell/EliminarGrupo.sh 
+				source /var/DataConfiguracionABMusuariosSO/sub_shell/listarGrupos.sh 
+				source /var/DataConfiguracionABMusuariosSO/sub_shell/Preferencias.sh 
+				carpetaBase='/var/DataConfiguracionABMusuariosSO'
+
+				respuesta="" #El dato pasado por los return solo puede ser numerico, entonces utilizamos una variable externa donde se cargen las salidas, como si fuera un $? pero con mayor capasidad 
+
+				echo "   _____________________________________________  "
+				echo "   |                                           | "
+				echo "   |                                           | "
+				echo "   |           ABM usuarios y grupos           | "
+				echo "   |                  por Bit                  | "
+				echo "   |                                           | "
+				echo "   |___________________________________________| "
+				echo "" 
+				# se carga un array con los nombre de las opciones del menu 
+				nombres=('Agregar_usuario' 'Modificar_usuarios' 'Eliminar_usuarios' 'Listar_usuarios' 'Agregar_grupo' 'editar_grupo' 'eliminar_grupo' 'Listar_grupo' 'Editar_preferencias' 'Reinstalar' 'Desinstalar')
+				# se carga el nombre de los metodos que llaman dichas opciones 
+				direcionesSetUp=('agregarUsuario' 'ModificarUsuario' 'eliminarUsuarios' 'listarUsuarios' 'agregarGrupo' 'ModificarGrupo' 'eliminarGrupo' 'MenuListarGrupos' 'Preferencias' 'ConfiguracionDelAmbienteDeTrabajo' 'desinstalar')
+
+				menu 'nombres[@]' 'direcionesSetUp[@]' #se llama al metodo menu 
+
+			else
+				echo "   _____________________________________________  "
+				echo "   |                                           | "
+				echo "   |                                           | "
+				echo "   |           ABM usuarios y grupos           | "
+				echo "   |                  por Bit                  | "
+				echo "   |                                           | "
+				echo "   |___________________________________________| "
+				echo "" 
+			
+				echo "debe instalar el softare para utilizarlo" 
+				echo "Desea comenzar el proseso de instalacion? (1= si 0=no)"
+				read de		
+				if test $de -eq 1 2> /dev/null #Comprueba que el dato ingresado sea 1
+				then
+					ConfiguracionDelAmbienteDeTrabajo #se prosesde con la instalacion del sistema 
+				fi	
+			fi
+		else
+			echo "Debe ser root para ejecutar este software"
 		fi
 	else
-		echo "Debe ser root para ejecutar este software"
-	fi
+		echo "Debe tener instalado Git para utilizar este shell "
+	fi	
 else
 	
 	echo "No puede ejecuar este shell script con el comando 'sh' use source o el nombre del archivo setup.sh"
