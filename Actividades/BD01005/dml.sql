24ae8bf32580bf491823383e8b60ca3dff506029
Daniel Padron <daniel2242014@gmail.com>
2019-06-19 03:35:08 -0300

    se agrega la entidad creadopor al mer y al ddl, se continua con el dml

--- Actividades/BD01005/dml.sql
+++ Actividades/BD01005/dml.sql
@@ -1,75 +1,24 @@
-truncate vehiculoingresa;
-truncate actualiza;
-truncate imagenregistro;
-truncate registrodanios;
-truncate informedanios;
-truncate trabajaen;
-truncate conduce;
-truncate rampascamion;
-truncate camion;
-truncate posicionestransporte;
-truncate transporta;
-truncate transporte;
-truncate posicionado;
-truncate integra;
-truncate lote;
-truncate vehiculo;
-truncate subzona;
-truncate zona;
-truncate lugar;
-truncate usuario;
+insert into rol(nombre) values("Operario");
+insert into rol(nombre) values("Transportista");
+insert into rol(nombre) values("Administrador");
+insert into usuario values (0,"Felipe3","","felip49@gmail.com", "27/8/1981", "09877745",
+"Felipe","","Camacho", "" , "Cual fue su primer juego" , "Lol" , 'M',3);
+insert into usuario values (0,"Fernanda1654","","ferti49@gmail.com", "25/2/1985", "098545574"
+,"Fernanda","Miranda","Lopes", "" , "Cual fue su primer telefono" , "Un poquet" , 'F',3);
+insert into usuario values (0,"Pepe12","","e1@gmail.com", "25/5/1925", "098167462"
+,"Pepe","Lorenzo","Miranda", "Lopes" , "Cual es tu color favorito" , "Rojo" , 'M',1);
+insert into usuario values (0,"Juan24","","re2@outlook.com", "25/7/1945", "098427894"
+,"Juan","Lorenzo","Simon", "Antonio" , "Cual es el nombre de tu perro" , "fido" , 'F',1);
+insert into usuario values (0,"Anto322","","anti@gmail.com", "27/4/1995", "098456782"
+,"Antonio","","PardiÃ±as", "" , "Cual es el nombre de su pelicula favorita" , "Blanca nieves" ,'O',2);
+insert into usuario values (0,"PedroB43","","Pedro43563@outlook.com", "2/9/1972", "098452746"
+,"Pedro","","Herandez", "Couto" , "El nomrbe de mi cancion favorita" , "Hello" , 'M',2);
 
-INSERT INTO
-	usuario (nombredeusuario, hash_contra, email, fechanac, telefono, primernombre, primerapellido, segundoapellido, preguntasecreta, respuestasecreta, sexo, rol)
-VALUES
-	("diosito", "no", "mvolcan@aguadita.uy", TO_DATE("1/1/1962", "%d/%m/%Y"), "911", "Miguel", "Volcan", "Sanchez", "Miau?", "Miau", "M", (select idrol from rol where nombre="Administrador"));
+insert into creadoPor values (2,1,"11/5/2019");
+insert into creadoPor values (3,1,"1/6/2019");
+insert into creadoPor values (4,2,"17/7/2019");
+insert into creadoPor values (5,2,"18/5/2019");
+insert into creadoPor values (6,2,"21/5/2019");
 
-INSERT INTO
-	usuario (nombredeusuario, hash_contra, email, fechanac, telefono, primernombre, primerapellido, segundoapellido, preguntasecreta, respuestasecreta, sexo, rol)
-VALUES
-	("koutakun", "no", "darkfm@vera.com.uy", TO_DATE("03/05/2001", "%d/%m/%Y"), "0305", "Salvador", "Pardiñas", "Barros", "Miau?", "Miau", "M", (select idrol from rol where nombre="Operario"));
-
-INSERT INTO
-	lugar (nombre, geox, geoy, usuariocreador, capacidad, tipo)
-VALUES
-	("Puerto de Montevideo", -35, -32, (select IDUsuario from usuario where NombreDeUsuario = "diosito"), 3, "Puerto");
-
-INSERT INTO
-	lugar (nombre, geox, geoy, usuariocreador, capacidad, tipo)
-VALUES
-	("Patio de Rocha", -35, -20, (select IDUsuario from usuario where NombreDeUsuario = "diosito"), 500, "Patio");
-
-INSERT INTO
-	zona (nombre, idlugar, capacidad)
-VALUES
-	("Rambla portuaria", (select IDLugar from lugar where nombre="Puerto de Montevideo"), 3);
-
-INSERT INTO
-	subzona (nombre, idzona, idlugar, capacidad)
-VALUES
-	("SZ-A", (select IDZona from zona where nombre="Rambla portuaria"), (select IDLugar from zona where nombre="Rambla portuaria"), 3);
-
-INSERT INTO
-	zona (nombre, idlugar, capacidad)
-VALUES
-	("Zona La Paloma", (select IDLugar from lugar where nombre = "Patio de Rocha"), 500);
-insert into
-	subzona (nombre, idzona, idlugar, capacidad)
-VALUES
-	("Subzona Arachania", (select IDZona from zona where nombre="Zona La Paloma"), (select IDLugar from zona where nombre="Zona La Paloma"), 500);
-
-insert into vehiculo (VIN, Marca, Modelo, Color, Tipo, Anio, ClienteNombre, PuertoArriba)
-values ("AUTARDO123", "Ford", "Focus", 0, "Auto", 2010, "Menem jr", (select IDLugar from lugar where nombre="Puerto de Montevideo"));
-
-insert into vehiculoingresa(VIN, fecha, tipoingreso, usuario)
-values ("AUTARDO123", TO_DATE("3/2/1991", "%d/%m/%Y"), "Alta", (select IDUsuario from usuario where NombreDeUsuario = "diosito"));
-
-
-insert into
-	lote (fechapartida, desde, hacia, creadorid, prioridad)
-values
-	(TO_DATE("5/3/1991", "%d/%m/%Y"), (select IDLugar from lugar where nombre="Puerto de Montevideo"), (select IDLugar from lugar where nombre="Patio de Rocha"), (select IDUsuario from usuario where nombredeusuario="diosito"), "Alta");
-insert into
-	integra(vin, lote, fecha, invalidado)
-values
-	("AUTARDO123", (select IDLote from lote), current, "f");
+insert into link values ("http://maps.com",5);
+insert into link values ("http://maps2.com",6);
