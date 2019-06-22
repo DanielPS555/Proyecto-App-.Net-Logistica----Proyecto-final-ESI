36ac2c4f10b83a437141f28700cc80c3ad2db6fd
koutakun <darkfm@vera.com.uy>
2019-06-22 15:38:35 -0300

    cambios

--- Actividades/Prog01002/Operario/Operario/Presentación/operario/panelInfoVehiculo.vb
+++ Actividades/Prog01002/Operario/Operario/Presentación/operario/panelInfoVehiculo.vb
@@ -4,24 +4,25 @@
         ' Esta llamada es exigida por el diseñador.
         Me.vin = VIN
         InitializeComponent()
-        ComboBox1.Items.Clear()
-        ComboBox1.Items.AddRange([Enum].GetNames(GetType(Logica.TipoVehiculo)))
+        TipoCombo.Items.Clear()
+        TipoCombo.Items.AddRange([Enum].GetNames(GetType(Logica.TipoVehiculo)))
         RegularTamañoColumnas()
         TomarValores()
     End Sub
 
     Private Sub TomarValores()
-        TextBox1.Text = vin
-        TextBox2.Text = VRepo.Marca(vin)
-        TextBox3.Text = VRepo.Modelo(vin)
-        TextBox4.Text = VRepo.Cliente(vin)
-        TextBox5.Text = VRepo.Año(vin)
-        ComboBox1.SelectedItem = VRepo.Tipo(vin)
+        VINBox.Text = vin
+        MarcaBox.Text = VRepo.Marca(vin)
+        ModeloBox.Text = VRepo.Modelo(vin)
+        ClienteBox.Text = VRepo.Cliente(vin)
+        AñoBox.Text = VRepo.Año(vin)
+        TipoCombo.SelectedItem = VRepo.Tipo(vin)
         Panel1.BackColor = VRepo.Color(vin)
-        Label20.Text = VRepo.Zona(vin)
-        Label21.Text = VRepo.Subzona(vin)
-        Label22.Text = VRepo.Posicion(vin)
-        Label23.Text = VRepo.Lote(vin)
+        ZonaLabel.Text = VRepo.Zona(vin)
+        SubzonaLabel.Text = VRepo.Subzona(vin)
+        PosicionLabel.Text = VRepo.Posicion(vin)
+        LoteCombo.Items.AddRange(LRepo.LotesEnLugar(VRepo.Lugar(vin)).ToArray)
+        LoteCombo.SelectedItem = VRepo.Lote(vin)
         informes.Columns.Clear()
         informes.DataSource = VRepo.Inspecciones(vin)
         traslados.Columns.Clear()
@@ -70,11 +71,17 @@
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        TextBox2.Enabled = Not TextBox2.Enabled
-        TextBox3.Enabled = Not TextBox3.Enabled
-        TextBox4.Enabled = Not TextBox4.Enabled
-        TextBox5.Enabled = Not TextBox5.Enabled
-        ComboBox1.Enabled = Not ComboBox1.Enabled
+        Dim permitido = (URepo.RolDeUsuario() = "Administrador")
+        If permitido Then
+            ModeloBox.Enabled = Not ModeloBox.Enabled
+            MarcaBox.Enabled = Not MarcaBox.Enabled
+        End If
+        permitido = permitido OrElse URepo.CreadorDe(vin)
+        If permitido Then
+            AñoBox.Enabled = Not AñoBox.Enabled
+            TipoCombo.Enabled = Not TipoCombo.Enabled
+        End If
+        LoteCombo.Enabled = Not LoteCombo.Enabled
         Button3.Visible = Not Button3.Visible
     End Sub
 
@@ -91,51 +98,51 @@
     Private _changedTB5 As Boolean = False
     Private _changedCB1 As Boolean = False
 
-    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles MarcaBox.TextChanged
         _changedTB2 = True
     End Sub
 
-    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles ModeloBox.TextChanged
         _changedTB3 = True
     End Sub
 
-    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles ClienteBox.TextChanged
         _changedTB4 = True
     End Sub
 
-    Private Sub TextBox5_TextChanged(sender As Object, e As EventArgs) Handles TextBox5.TextChanged
-        If TextBox5.Text.Where(Function(x) x >= "0" And x <= "9").Count <> TextBox5.Text.Count Then
-            TextBox5.ForeColor = Color.Red
+    Private Sub TextBox5_TextChanged(sender As Object, e As EventArgs) Handles AñoBox.TextChanged
+        If AñoBox.Text.Where(Function(x) x >= "0" And x <= "9").Count <> AñoBox.Text.Count Then
+            AñoBox.ForeColor = Color.Red
         Else
-            TextBox5.ForeColor = Color.Black
+            AñoBox.ForeColor = Color.Black
             _changedTB5 = True
         End If
     End Sub
 
-    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles TipoCombo.SelectedIndexChanged
         _changedCB1 = True
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         If _changedTB2 Then
             _changedTB2 = False
-            VRepo.Marca(vin, TextBox2.Text)
+            VRepo.Marca(vin, MarcaBox.Text)
         End If
         If _changedTB3 Then
             _changedTB3 = False
-            VRepo.Modelo(vin, TextBox3.Text)
+            VRepo.Modelo(vin, ModeloBox.Text)
         End If
-        If _changedTB5 And TextBox5.ForeColor = Color.Black Then
+        If _changedTB5 And AñoBox.ForeColor = Color.Black Then
             _changedTB5 = False
-            VRepo.Año(vin, TextBox5.Text)
+            VRepo.Año(vin, AñoBox.Text)
         End If
         If _changedTB4 Then
             _changedTB4 = False
-            VRepo.Cliente(vin, TextBox4.Text)
+            VRepo.Cliente(vin, ClienteBox.Text)
         End If
         If _changedCB1 Then
             _changedCB1 = False
-            VRepo.Tipo(vin, ComboBox1.SelectedItem)
+            VRepo.Tipo(vin, TipoCombo.SelectedItem)
         End If
         Button1_Click(sender, e)
     End Sub
