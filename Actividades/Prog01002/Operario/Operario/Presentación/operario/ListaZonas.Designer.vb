6f99f257cf4f5c4c728d9b207f1af63d40440a99
ESI BUCEO <esi-buceo@outlook.com>
2019-06-24 18:22:13 -0300

    Formato base del panel de zona

--- Actividades/Prog01002/Operario/Operario/Presentación/operario/ListaZonas.Designer.vb
+++ Actividades/Prog01002/Operario/Operario/Presentación/operario/ListaZonas.Designer.vb
@@ -22,98 +22,121 @@ Partial Class ListaZonas
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
-        Me.NombreLabel = New System.Windows.Forms.Label()
-        Me.zonalbl = New System.Windows.Forms.Label()
-        Me.LugarLbl = New System.Windows.Forms.Label()
-        Me.childList = New System.Windows.Forms.ListBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'TreeView1
+        'ListBox1
         '
-        Me.TreeView1.Location = New System.Drawing.Point(12, 12)
-        Me.TreeView1.Name = "TreeView1"
-        Me.TreeView1.Size = New System.Drawing.Size(257, 626)
-        Me.TreeView1.TabIndex = 0
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 36)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(215, 277)
+        Me.ListBox1.TabIndex = 0
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.BackgroundColor = System.Drawing.Color.White
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(244, 358)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(624, 283)
+        Me.DataGridView1.TabIndex = 1
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(381, 98)
+        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(248, 36)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(61, 13)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Capacidad:"
+        Me.Label1.Size = New System.Drawing.Size(164, 22)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Datos de la zona"
         '
-        'ProgressBar1
+        'Label2
         '
-        Me.ProgressBar1.Location = New System.Drawing.Point(275, 98)
-        Me.ProgressBar1.Name = "ProgressBar1"
-        Me.ProgressBar1.Size = New System.Drawing.Size(100, 23)
-        Me.ProgressBar1.TabIndex = 2
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Century Gothic", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(248, 181)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(196, 22)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Datos de la subzona"
         '
-        'NombreLabel
+        'Label3
         '
-        Me.NombreLabel.AutoSize = True
-        Me.NombreLabel.Location = New System.Drawing.Point(275, 12)
-        Me.NombreLabel.Name = "NombreLabel"
-        Me.NombreLabel.Size = New System.Drawing.Size(47, 13)
-        Me.NombreLabel.TabIndex = 3
-        Me.NombreLabel.Text = "Nombre:"
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Century Gothic", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 13)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(56, 22)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Zona"
         '
-        'zonalbl
+        'Label4
         '
-        Me.zonalbl.AutoSize = True
-        Me.zonalbl.Location = New System.Drawing.Point(275, 25)
-        Me.zonalbl.Name = "zonalbl"
-        Me.zonalbl.Size = New System.Drawing.Size(35, 13)
-        Me.zonalbl.TabIndex = 4
-        Me.zonalbl.Text = "Zona:"
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Century Gothic", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 334)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(94, 22)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Subzona "
         '
-        'LugarLbl
+        'ListBox2
         '
-        Me.LugarLbl.AutoSize = True
-        Me.LugarLbl.Location = New System.Drawing.Point(275, 38)
-        Me.LugarLbl.Name = "LugarLbl"
-        Me.LugarLbl.Size = New System.Drawing.Size(34, 13)
-        Me.LugarLbl.TabIndex = 5
-        Me.LugarLbl.Text = "Lugar"
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(12, 359)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(215, 277)
+        Me.ListBox2.TabIndex = 6
         '
-        'childList
+        'Label5
         '
-        Me.childList.FormattingEnabled = True
-        Me.childList.Location = New System.Drawing.Point(278, 127)
-        Me.childList.Name = "childList"
-        Me.childList.Size = New System.Drawing.Size(590, 511)
-        Me.childList.TabIndex = 6
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Century Gothic", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(248, 333)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(96, 22)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Vehiculo "
         '
         'ListaZonas
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(880, 650)
-        Me.Controls.Add(Me.childList)
-        Me.Controls.Add(Me.LugarLbl)
-        Me.Controls.Add(Me.zonalbl)
-        Me.Controls.Add(Me.NombreLabel)
-        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.TreeView1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.ListBox1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "ListaZonas"
         Me.Text = "ListaZonas"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents TreeView1 As TreeView
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents DataGridView1 As DataGridView
     Friend WithEvents Label1 As Label
-    Friend WithEvents ProgressBar1 As ProgressBar
-    Friend WithEvents NombreLabel As Label
-    Friend WithEvents zonalbl As Label
-    Friend WithEvents LugarLbl As Label
-    Friend WithEvents childList As ListBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents Label5 As Label
 End Class
--- Actividades/Prog01002/Operario/Operario/Presentación/operario/ListaZonas.vb
+++ Actividades/Prog01002/Operario/Operario/Presentación/operario/ListaZonas.vb
@@ -5,86 +5,92 @@
         InitializeComponent()
 
         ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
-        CargarData()
+        'CargarData()
     End Sub
 
-    Private conectadoEn As String
-    Private Sub CargarData()
-        TreeView1.Nodes.Clear()
-        conectadoEn = URepo.ConectadoEn
-        Dim zonas = LRepo.ZonasEnLugar(URepo.ConectadoEn)
-        zonas.ForEach(Sub(x)
-                          Dim subzonas = LRepo.SubzonasEnLugar(x, conectadoEn)
-                          Dim n = TreeView1.Nodes.Add(x)
-                          For Each y In subzonas
-                              Dim vehiculos As List(Of String) = LRepo.VehiculosEnSubzona(y, x, conectadoEn)
-                              Dim t = n.Nodes.Add(y)
-                              For Each v In vehiculos
-                                  t.Nodes.Add(v)
-                              Next
-                          Next
-                      End Sub)
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
     End Sub
 
-    Private Sub TreeView1_AfterSelect(sender As Object, e As TreeViewEventArgs) Handles TreeView1.AfterSelect
-        If e.Node Is Nothing Then
-            Label1.Text = ""
-            childList.Items.Clear()
-            Return
-        End If
-        If pInfo IsNot Nothing Then
-            pInfo.Close()
-            Marco.getInstancia.cerrarPanel(Of panelInfoVehiculo)()
-        End If
-        If e.Node.Level = 0 Then
-            Label1.Visible = True
-            ProgressBar1.Visible = True
-            Dim capacidad As Integer = LRepo.CapacidadZona(e.Node.Text, conectadoEn)
-            Dim ocupacion As Integer = LRepo.OcupacionZona(e.Node.Text, conectadoEn)
-            Label1.Text = $"Capacidad: ({ocupacion}/{capacidad})"
-            ProgressBar1.Maximum = capacidad
-            ProgressBar1.Value = ocupacion
-            childList.Items.Clear()
-        ElseIf e.Node.Level = 1 Then
-            Label1.Visible = True
-            ProgressBar1.Visible = True
-            Dim capacidad As Integer = LRepo.CapacidadSubzona(e.Node.Text, e.Node.Parent.Text, conectadoEn)
-            Dim ocupacion As Integer = LRepo.OcupacionSubzona(e.Node.Text, e.Node.Parent.Text, conectadoEn)
-            Label1.Text = $"Capacidad: ({ocupacion}/{capacidad})"
-            ProgressBar1.Maximum = capacidad
-            ProgressBar1.Value = ocupacion
-        ElseIf e.Node.Level = 2 Then
-            Label1.Visible = False
-            ProgressBar1.Visible = False
-            pInfo = New panelInfoVehiculo(e.Node.Text, True)
-            Marco.getInstancia.cargarPanel(pInfo)
-            pInfo.Location = Label1.Location
-        End If
-        If e.Node.Level < 2 Then
-            childList.Items.Clear()
-            For Each i As TreeNode In e.Node.Nodes
-                childList.Items.Add(i.Text)
-            Next
-        End If
-    End Sub
-    Private pInfo As panelInfoVehiculo
 
-    Private Sub ListaZonas_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
-        If pInfo IsNot Nothing Then
-            pInfo.Close()
-            Marco.getInstancia.cerrarPanel(Of panelInfoVehiculo)()
-        End If
-    End Sub
 
-    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles childList.SelectedIndexChanged
-        If childList.SelectedIndex < 0 Then
-            Return
-        End If
-        For Each i As TreeNode In TreeView1.SelectedNode.Nodes
-            If i.Text = childList.SelectedItem Then
-                TreeView1.SelectedNode = i
-                Return
-            End If
-        Next
-    End Sub
+    'Private conectadoEn As String
+    'Private Sub CargarData()
+    '    TreeView1.Nodes.Clear()
+    '    conectadoEn = URepo.ConectadoEn
+    '    Dim zonas = LRepo.ZonasEnLugar(URepo.ConectadoEn)
+    '    zonas.ForEach(Sub(x)
+    '                      Dim subzonas = LRepo.SubzonasEnLugar(x, conectadoEn)
+    '                      Dim n = TreeView1.Nodes.Add(x)
+    '                      For Each y In subzonas
+    '                          Dim vehiculos As List(Of String) = LRepo.VehiculosEnSubzona(y, x, conectadoEn)
+    '                          Dim t = n.Nodes.Add(y)
+    '                          For Each v In vehiculos
+    '                              t.Nodes.Add(v)
+    '                          Next
+    '                      Next
+    '                  End Sub)
+    'End Sub
+
+    'Private Sub TreeView1_AfterSelect(sender As Object, e As TreeViewEventArgs) Handles TreeView1.AfterSelect
+    '    If e.Node Is Nothing Then
+    '        Label1.Text = ""
+    '        childList.Items.Clear()
+    '        Return
+    '    End If
+    '    If pInfo IsNot Nothing Then
+    '        pInfo.Close()
+    '        Marco.getInstancia.cerrarPanel(Of panelInfoVehiculo)()
+    '    End If
+    '    If e.Node.Level = 0 Then
+    '        Label1.Visible = True
+    '        ProgressBar1.Visible = True
+    '        Dim capacidad As Integer = LRepo.CapacidadZona(e.Node.Text, conectadoEn)
+    '        Dim ocupacion As Integer = LRepo.OcupacionZona(e.Node.Text, conectadoEn)
+    '        Label1.Text = $"Capacidad: ({ocupacion}/{capacidad})"
+    '        ProgressBar1.Maximum = capacidad
+    '        ProgressBar1.Value = ocupacion
+    '        childList.Items.Clear()
+    '    ElseIf e.Node.Level = 1 Then
+    '        Label1.Visible = True
+    '        ProgressBar1.Visible = True
+    '        Dim capacidad As Integer = LRepo.CapacidadSubzona(e.Node.Text, e.Node.Parent.Text, conectadoEn)
+    '        Dim ocupacion As Integer = LRepo.OcupacionSubzona(e.Node.Text, e.Node.Parent.Text, conectadoEn)
+    '        Label1.Text = $"Capacidad: ({ocupacion}/{capacidad})"
+    '        ProgressBar1.Maximum = capacidad
+    '        ProgressBar1.Value = ocupacion
+    '    ElseIf e.Node.Level = 2 Then
+    '        Label1.Visible = False
+    '        ProgressBar1.Visible = False
+    '        pInfo = New panelInfoVehiculo(e.Node.Text, True)
+    '        Marco.getInstancia.cargarPanel(pInfo)
+    '        pInfo.Location = Label1.Location
+    '    End If
+    '    If e.Node.Level < 2 Then
+    '        childList.Items.Clear()
+    '        For Each i As TreeNode In e.Node.Nodes
+    '            childList.Items.Add(i.Text)
+    '        Next
+    '    End If
+    'End Sub
+    'Private pInfo As panelInfoVehiculo
+
+    'Private Sub ListaZonas_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+    '    If pInfo IsNot Nothing Then
+    '        pInfo.Close()
+    '        Marco.getInstancia.cerrarPanel(Of panelInfoVehiculo)()
+    '    End If
+    'End Sub
+
+    'Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles childList.SelectedIndexChanged
+    '    If childList.SelectedIndex < 0 Then
+    '        Return
+    '    End If
+    '    For Each i As TreeNode In TreeView1.SelectedNode.Nodes
+    '        If i.Text = childList.SelectedItem Then
+    '            TreeView1.SelectedNode = i
+    '            Return
+    '        End If
+    '    Next
+    'End Sub
 End Class
