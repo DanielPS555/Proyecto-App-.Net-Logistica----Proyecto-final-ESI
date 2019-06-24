f2bbf14cc48a7bf57c02c5f939b0ccb5f7fa59a5
koutakun <darkfm@vera.com.uy>
2019-06-23 21:35:21 -0300

    uuuuuuuu

--- Actividades/Prog01002/Operario/Operario/Presentación/operario/PanelInfoLote.Designer.vb
+++ Actividades/Prog01002/Operario/Operario/Presentación/operario/PanelInfoLote.Designer.vb
@@ -22,9 +22,67 @@ Partial Class PanelInfoLote
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nombre:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 22)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(46, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Destino:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 55)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(33, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Fase:"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(15, 71)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(497, 161)
+        Me.DataGridView1.TabIndex = 4
+        '
+        'PanelInfoLote
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(864, 611)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "PanelInfoLote"
         Me.Text = "PanelInfoLote"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents DataGridView1 As DataGridView
 End Class
--- Actividades/Prog01002/Operario/Operario/Presentación/operario/PanelInfoLote.vb
+++ Actividades/Prog01002/Operario/Operario/Presentación/operario/PanelInfoLote.vb
@@ -1,3 +1,14 @@
-﻿Public Class PanelInfoLote
+﻿Imports Operario.Logica
 
+Public Class PanelInfoLote
+    Public Sub New(l As Logica.Lote)
+
+        ' Esta llamada es exigida por el diseñador.
+        InitializeComponent()
+        Label1.Text = l.Nombre
+        Label2.Text = l.Hacia.Nombre
+        Label3.Text = [Enum].GetName(GetType(EstadoLote), l.Estado)
+        ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
+        DataGridView1.DataSource = LRepo.VehiculosEnLote(l.ID)
+    End Sub
 End Class
