6cc019302a3060469ef289a72f89a9a5488eeb5d
ESI BUCEO <esi-buceo@outlook.com>
2019-08-05 15:14:14 -0300

    demele dedele generador de VINs

---
+++ Actividades/BD01005/dml2.sql
@@ -0,0 +1,13 @@
+insert into vehiculo(vin,cliente,tipo) values('0JYSOO7SMR54BVV13', (select IDCliente from cliente where nombre='Chevrolet UY'), 'Auto');
+insert into vehiculo(vin,cliente,tipo) values('0B8FO84BSNC5M14RG', (select IDCliente from cliente where nombre='Sevel'), 'Auto');
+insert into vehiculo(vin,cliente,tipo) values('ETVWXUQI139EKGZDE', (select IDCliente from cliente where nombre='Chevrolet UY'), 'Auto');
+insert into vehiculo(vin,cliente,tipo) values('DLVQOCVKGH54ZKFMM', (select IDCliente from cliente where nombre='Sevel'), 'Auto');
+insert into vehiculo(vin,cliente,tipo) values('Y3EDP5602E2V58NED', (select IDCliente from cliente where nombre='Chevrolet UY'), 'Auto');
+insert into vehiculo(vin,cliente,tipo) values('DN4I1CE8CX3OZXTPF', (select IDCliente from cliente where nombre='Sevel'), 'Auto');
+insert into vehiculo(vin,cliente,tipo) values('B0WPKOAHHKBXGL7J9', (select IDCliente from cliente where nombre='Chevrolet UY'), 'Auto');
+insert into vehiculo(vin,cliente,tipo) values('4F21ZGS8WDF52GSD2', (select IDCliente from cliente where nombre='Sevel'), 'Auto');
+insert into vehiculo(vin,cliente,tipo) values('ZN2G8KVTXFHHHFNTL', (select IDCliente from cliente where nombre='Chevrolet UY'), 'Auto');
+insert into vehiculo(vin,cliente,tipo) values('8A1FWCRW1BIV43JUJ', (select IDCliente from cliente where nombre='Chevrolet UY'), 'Auto');
+insert into vehiculo(vin,cliente,tipo) values('WO7I65ICREVZI8VS4', (select IDCliente from cliente where nombre='Chevrolet UY'), 'Auto');
+insert into vehiculo(vin,cliente,tipo) values('BJRQOCIJB1BEYNW1X', (select IDCliente from cliente where nombre='Chevrolet UY'), 'Auto');
+insert into vehiculo(vin,cliente,tipo) values('SP3VWJLD01T90BYRI', (select IDCliente from cliente where nombre='Chevrolet UY'), 'Auto');
