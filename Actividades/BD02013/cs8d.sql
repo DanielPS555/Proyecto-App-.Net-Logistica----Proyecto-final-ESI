9166ef6de994c4e70ae080920b0bd025f162e0f3
Koutarou <kouta@localhost.localdomain>
2019-08-07 22:52:06 -0300

    soy mogolico

--- Actividades/BD02013/cs8d.sql
+++ Actividades/BD02013/cs8d.sql
@@ -4,9 +4,9 @@ merge into actualiza as dst
 		?::integer as informe2, ?::integer as registro2,
 		?::varchar(15) as tipo from sysmaster:'informix'.sysdual
 		) as src
-	on dst.vehiculo1=src.vehiculo1 and dst.vehiculo2=src.vehiculo2
-	and dst.informe1=src.informe1 and dst.informe2=src.informe2
-	and dst.registro1=src.registro1 and dst.registro2=src.registro2
+	on dst.vehiculo1=src.vehiculo1
+	and dst.informe1=src.informe1
+	and dst.registro1=src.registro1
 when not matched then insert
 	(dst.vehiculo1, dst.informe1, dst.registro1,
 	 dst.vehiculo2, dst.informe2, dst.registro2,
@@ -14,4 +14,5 @@ when not matched then insert
 	(src.vehiculo1, src.informe1, src.registro1,
 	 src.vehiculo2, src.informe2, src.registro2,
 	 src.tipo)
-when matched then update set dst.tipo=src.tipo;
+when matched then update set dst.tipo=src.tipo, dst.registro2=src.registro2,
+	dst.informe2=src.informe2;
