a26c025506d336cbd9a0209edef7960ffc15b374
Daniel <Daniel2242014@gmail.com>
2019-08-11 16:33:20 -0300

    Erradicación total de la anterior arquitectura

--- Actividades/Prog01002/Operario/Operario/Lógica/Role.vb
+++
@@ -1,8 +0,0 @@
-﻿Namespace Logica
-    Public Class Role
-        Public ReadOnly Nombre As String
-        Protected Friend Sub New(Nombre As String)
-            Me.Nombre = Nombre
-        End Sub
-    End Class
-End Namespace
