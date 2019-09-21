10e1fe1b2ba8b8050858685cd05433e7b5040d8a
Daniel <Daniel2242014@gmail.com>
2019-09-21 16:23:37 -0300

    Incio de transportista

---
+++ Actividades/Prog01002/Operario/TransportistaV2/BootstrapTransportista.vb
@@ -0,0 +1,19 @@
+﻿Imports TransportistaCore
+Imports Controladores
+
+Public Class BootstrapTransportista
+    Public Shared Sub Main()
+        Funciones_comunes.Inter_test()
+        Dim p = Controladores.Principal.CrearInstancia(Sub()
+                                                           Dim paneles = New Dictionary(Of String, Type) From {
+                                                 {"Lista de Lotes", GetType(TransportistaCore.Lista_de_trasportes)},
+                                                 {"Lista de transportes", GetType(TransportistaCore.ListaDeTrasportes)},
+                                                 {"Medios autorizados", GetType(TransportistaCore.ListaDeMediosAutorizados)}
+                                             }
+                                                           Controladores.Marco.SetButtons(paneles)
+                                                           Marco.ReiniciarSingleton()
+                                                           Principal.getInstancia.cargarPanel(Marco.getInstancia())
+                                                       End Sub, Usuario.TIPO_ROL_TRANSPORTISTA)
+        p.ShowDialog()
+    End Sub
+End Class
--- Actividades/Prog01002/Operario/TransportistaV2/My Project/Settings.Designer.vb
+++ Actividades/Prog01002/Operario/TransportistaV2/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.Trasportista.My.MySettings
+        Friend ReadOnly Property Settings() As Global.TransportistaV2.My.MySettings
             Get
-                Return Global.Trasportista.My.MySettings.Default
+                Return Global.TransportistaV2.My.MySettings.Default
             End Get
         End Property
     End Module
