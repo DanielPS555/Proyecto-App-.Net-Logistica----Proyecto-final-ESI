ae83d85d9c583c32426518fe3c4d0a0108dcb075
koutakun <darkfm@vera.com.uy>
2019-09-29 16:01:19 -0300

    dml

--- Actividades/BD01005/propuesta_wc_dml.sql
+++ Actividades/BD01005/propuesta_wc_dml.sql
@@ -1,3 +1,7 @@
 insert into evento(id, datos, fechaAgregado) values(0,
 '{"tipo": "comentario", "por": "admin", "autor": 1, "idvehiculo": 1, "mensaje": "Está en la zona"}'::json
 , current year to second);
+
+insert into evento(datos, fechaAgregado) values(0,
+'{"tipo": "comentario", "por": "cliente", "autor": 1, "idvehiculo": 1, "mensaje": "Grasia don!"}'::json
+, current year to second);
