36d5475d1103bb79c8ba0ef0514c61428c73ed49
ESI BUCEO <esi-buceo@outlook.com>
2019-10-29 16:22:32 -0300

    crear paquetes

--- Actividades/Prog01003/Instalador/CrearPaquete/Form1.Designer.cs
+++ Actividades/Prog01003/Instalador/CrearPaquete/Form1.Designer.cs
@@ -69,6 +69,7 @@
             this.button1.TabIndex = 3;
             this.button1.Text = "Generar Conditions.txt";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -78,6 +79,7 @@
             this.button2.TabIndex = 4;
             this.button2.Text = "Generar zip";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // conditions
             // 
