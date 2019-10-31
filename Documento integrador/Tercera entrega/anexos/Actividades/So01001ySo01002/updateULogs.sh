08221020a06716001dabdb2d180c3f7c916a9a67
ESI BUCEO <esi-buceo@outlook.com>
2019-10-31 16:57:58 -0300

    Carpeta de anexos final

--- Documento integrador/Tercera entrega/anexos/Actividades/So01001ySo01002/updateULogs.sh
+++
@@ -1,4 +0,0 @@
-echo "EXITOSOS:" > /var/SLTAUserLog
-last Administrador Operario Transportista >> /var/SLTAUserLog
-echo "FALLIDOS:" >> /var/SLTAUserLog
-lastb Administrador Operario Transportista >> /var/SLTAUserLog
